978ddd05fe1c974405d904e50c067cd00868edb7
GABETROLL <gabriel7icplhappy@gmail.com>
2023-08-10 11:02:45 -0400

    Add correct arg count

--- markdown2html.py
+++ markdown2html.py
@@ -7,6 +7,8 @@ Usage: ./markdown2html.py <MD input file> <HTML output file>
 from sys import argv, stderr, exit
 
 if __name__ == "__main__":
+    assert argv
+
     if len(argv) < 3:
         print(f"Usage: {argv[0]} README.md README.html", file=stderr)
         exit(1)
