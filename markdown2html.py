5729c5936559bccd052cc3b49d43a68729d873c7
GABETROLL <gabriel7icplhappy@gmail.com>
2023-08-10 14:01:12 -0400

    Re-structure code to make proper highlighting

--- markdown2html.py
+++ markdown2html.py
@@ -7,6 +7,36 @@ Usage: ./markdown2html.py <MD input file> <HTML output file>
 from sys import argv, stderr, exit
 
 
+def decorate_line(line: str, html_output_file):
+    """
+    Returns a new string like line,
+    but with its mardown decorations ("**...**", "__...__")
+    as their corresponding HTML tags (<b>...</b>, <em></em>)
+
+    Using a stack, to allow for nested decorations.
+    
+    INVALID OPENING AND CLOSING DECORATIONS BREAK THIS FUNCTION!!
+    """
+    decoration_stack = []
+    index = 0
+
+    result = line
+
+    while index < len(line):
+        for md, html in {"**": "b", "__": "em"}.items():
+            if line.startswith(md, index):
+                if decoration_stack == [] or decoration_stack[-1] != md:
+                    decoration_stack.append(md)
+                    result = result[:index] + f"<{html}>" + result[index + len(md):]
+                else:
+                    decoration_stack.pop()
+                    result = result[:index] + f"</{html}>" + result[index + len(md):]
+                index += 2
+                break
+        else:
+            index += 1
+
+
 if __name__ == "__main__":
     assert argv
 
@@ -27,7 +57,17 @@ if __name__ == "__main__":
 
     MD_INPUT_FILE_ITER = iter(MD_INPUT_FILE)
 
+    previous_line_type: str = ""
+
     for line in MD_INPUT_FILE_ITER:
+
+        # Close opened tags, if any
+        if previous_line_type == "p" and not line[:-1]:
+            HTML_OUTPUT_FILE.write("</p>\n")
+        elif previous_line_type == "ul" and not line.startswith("- "):
+            HTML_OUTPUT_FILE.write("</ul>\n")
+        elif previous_line_type == "ol" and not line.startswith("* "):
+            HTML_OUTPUT_FILE.write("</ol>\n")
 
         if line.startswith("#"):
 
@@ -38,70 +78,34 @@ if __name__ == "__main__":
 
                     REST_OF_LINE = line[hashtag_amount + 1:].strip()
                     HTML_OUTPUT_FILE.write(f"<h{hashtag_amount}>{REST_OF_LINE}</h{hashtag_amount}>\n")
+
+                    previous_line_type = "h"
+                    break
+            else:
+                if previous_line_type == "p":
+                    HTML_OUTPUT_FILE.write("<br>")
+                else:
+                    HTML_OUTPUT_FILE.write("<p>")
+
+                HTML_OUTPUT_FILE.write(line.strip())
+                previous_line_type = "p"
+
         elif line.startswith("- ") or line.startswith("* "):
 
             for list_line_start, list_tag in {"- ": "ul", "* ": "ol"}.items():
                 if line.startswith(list_line_start):
 
-                    # Write the opening 'list_tag', then
-                    # continue to iterate through the next 'line's
-                    # Using next(MD_INPUT_FILE_ITER),
-                    # until the lines beginning with 'list_line_start' are over,
-                    # and we can then write the closing 'list_tag', then a new line.
+                    if previous_line_type != list_tag:
+                        HTML_OUTPUT_FILE.write(f"<{list_tag}>\n{line.strip()}")
 
-                    HTML_OUTPUT_FILE.write(f"<{list_tag}>\n")
+                    previous_line_type = list_tag
+        elif line[:-1]:
+            if previous_line_type == "p":
+                HTML_OUTPUT_FILE.write("<br>")
+            else:
+                HTML_OUTPUT_FILE.write("<p>")
 
-                    while line.startswith(list_line_start):
-
-                        HTML_OUTPUT_FILE.write("<li>" + line[len(list_line_start):].strip() + "</li>\n")
-
-                        # move on to next line HERE
-                        try:
-                            line = next(MD_INPUT_FILE_ITER)
-                        except StopIteration:
-                            break
-
-                    HTML_OUTPUT_FILE.write(f"</{list_tag}>\n")
-        elif line.strip():
-            HTML_OUTPUT_FILE.write("<p>")
-
-            while line.strip():
-                decoration_stack = []
-                index = 0
-
-                STRIPPED_LINE = line.strip()
-
-                while index < len(STRIPPED_LINE):
-                    if STRIPPED_LINE.startswith("**", index):
-
-                        if decoration_stack == [] or decoration_stack[-1] != "**":
-                            decoration_stack.append("**")
-                            HTML_OUTPUT_FILE.write("<b>")
-                        else:
-                            decoration_stack.pop()
-                            HTML_OUTPUT_FILE.write("</b>")
-                        index += 2
-                    elif STRIPPED_LINE.startswith("__", index):
-                        if decoration_stack == [] or decoration_stack[-1] != "__":
-                            decoration_stack.append("__")
-                            HTML_OUTPUT_FILE.write("<em>")
-                        else:
-                            decoration_stack.pop()
-                            HTML_OUTPUT_FILE.write("</em>")
-                        index += 2
-                    else:
-                        HTML_OUTPUT_FILE.write(STRIPPED_LINE[index])
-                        index += 1
-
-                # move on to next line HERE
-                try:
-                    line = next(MD_INPUT_FILE_ITER)
-                except StopIteration:
-                    break
-
-                if line.strip():
-                    HTML_OUTPUT_FILE.write("<br>")
-
-            HTML_OUTPUT_FILE.write("</p>\n")
+            HTML_OUTPUT_FILE.write(line.strip())
+            previous_line_type = "p"
 
     exit(0)
--- tests/input.md
+++ tests/input.md
@@ -1,6 +1,20 @@
-**hello**
-**HAI**
-__WHAT__
-__IS__
-   __GOING__
-     __ON__   **HI AGAIN**
+# Welcome __to__ Holberton
+
+Nice school!
+
+- Web**stack**
+- AR/VR
+- ML
+- Low level
+- __and more__
+
+It's a **super __cool__** project.
+Like really. no?
+
+## Tasks
+
+* read __**requirements**__
+* quiz time
+* tasks
+
+In this project you will learn how to translate a 
